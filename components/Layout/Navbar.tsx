cfcfe99313575663dbfdb4cebcc2dcd0de649185
Neamul01 <neamulhaque2002@gmail.com>
2023-10-12 03:04:52 +0600

    configuration added

---
+++ components/Layout/Navbar.tsx
@@ -0,0 +1,131 @@
+"use client";
+
+import * as React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
+const drawerWidth = 240;
+const navItems = ["Home", "About", "Contact"];
+
+function Navbar(props: any) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        MUI
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <div className="max-w-layout xl:max-w-widerInner mx-auto">
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar component="nav" className="bg-primary">
+          <div className="md:w-layout xl:w-wider w-full mx-auto">
+            <Toolbar>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                edge="start"
+                onClick={handleDrawerToggle}
+                sx={{ mr: 2, display: { sm: "none" } }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography
+                variant="h6"
+                component="div"
+                sx={{ flexGrow: 1, display: { sm: "block" } }}
+                className="text-right md:text-left"
+              >
+                MUI
+              </Typography>
+              <Box sx={{ display: { xs: "none", sm: "block" } }}>
+                {navItems.map((item) => (
+                  <Button key={item} sx={{ color: "#fff" }}>
+                    {item}
+                  </Button>
+                ))}
+              </Box>
+              {/* <Box sx={{ display: { xs: "block", sm: "none" } }} >Icon</Box> */}
+            </Toolbar>
+          </div>
+        </AppBar>
+        <nav>
+          <Drawer
+            container={container}
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+            sx={{
+              display: { xs: "block", sm: "none" },
+              "& .MuiDrawer-paper": {
+                boxSizing: "border-box",
+                width: drawerWidth,
+              },
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </nav>
+        <Box component="main" sx={{ p: 3 }}>
+          <Toolbar />
+          <Typography>
+            Quidem alias repudiandae eaque eveniet cumque nihil aliquam in
+            expedita, impedit quas ipsum nesciunt ipsa ullam consequuntur
+            dignissimos numquam at nisi porro a, quaerat rem repellendus.
+            Voluptates perspiciatis, in pariatur impedit, nam facilis libero
+            dolorem dolores sunt inventore perferendis, aut sapiente modi
+            nesciunt.
+          </Typography>
+        </Box>
+      </Box>
+    </div>
+  );
+}
+
+Navbar.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default Navbar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,33 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        primary: "#4a30fe",
+        secondary: "#ff0e85",
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        primary: "#4a30fe",
+        secondary: "#ff0e85",
+      },
+      maxWidth: {
+        layout: "1024px",
+        wider: "1220px",
+        widerInner: "1120px",
+      },
+      width: {
+        layout: "1024px",
+        wider: "1220px",
+        widerInner: "1120px",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
